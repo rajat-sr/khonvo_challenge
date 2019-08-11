e658e49679f2358999acc760d149ff182c727f02
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-08-11 17:15:12 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Querier from './components/Querier';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Khonvo
+        </p>
+        <Querier/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Querier.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+class Querier extends Component {
+  render () {
+    return (
+      <div>
+        <Button type="primary" size="large">Hurray!</Button>
+      </div>
+    )
+  }
+}
+
+export default Querier
