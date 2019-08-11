8ee7c1d97ccf9656acbe7af9a1235d6ed2198b49
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-08-12 02:03:21 +0530

    feat: Add GET /api/job/:id route

--- routes/jobApi.js
+++ routes/jobApi.js
@@ -4,6 +4,41 @@ const Job = require('../models/job');
 
 const router = express.Router();
 
+// Get all jobs
+router.get('/', async (req, res, next) => {
+  let jobs;
+  try {
+    jobs = await Job.find();
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+
+  return res.status(200).send(jobs);
+});
+
+// Get a job by its id
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send('Invalid job id');
+  }
+
+  let jobs;
+  try {
+    jobs = await Job.findById(id);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+
+  if (!jobs) {
+    return res.status(404).send('Not found');
+  }
+
+  return res.status(200).send(jobs);
+});
+
+// Add a new job
 router.post('/', async (req, res, next) => {
   const {
     companyName,
