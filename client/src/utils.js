33bf8e70615eda5b131191eb3e141dee544b6b17
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-08-13 16:57:47 +0530

    chore: Add Heroku configs

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV === 'PRODUCTION' ? '' : 'http://localhost:5000/api';
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://khonvo-challenge.herokuapp.com/api' : 'http://localhost:5000/api';
 
 module.exports = {
   BASE_URL,
